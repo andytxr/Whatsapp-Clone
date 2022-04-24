4e6f457588913f83cab2fcf302a2d028a9424581
decosawa <andreluiiz.gst@gmail.com>
2022-04-24 20:39:16 -0300

    Funcionalide de utilizar a câmera

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -8,6 +8,7 @@ export class CameraController{
         }).then(stream=>{
 
             let mediaStream = new MediaStream(stream);
+            this._stream=stream;
             this._videoEl.srcObject = mediaStream;
             this._videoEl.play();
 
@@ -16,6 +17,30 @@ export class CameraController{
             console.error(err)
 
         })
+    }
+
+    stopCamera(){
+
+        this._stream.getTracks().forEach(track=>{
+
+            track.stop();
+
+        })
+
+    }
+
+    takePicture(mimeType='image/png'){
+
+        let canvas = document.createElement('canvas');
+
+        canvas.setAttribute('height', this._videoEl.videoHeight);
+        canvas.setAttribute('width', this._videoEl.videoWidth);
+
+        let context = canvas.getContext('2d');
+        context.drawImage(this._videoEl, 0, 0, canvas.width, canvas.height);
+
+        return canvas.toDataURL(mimeType);
+
     }
 
 }
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -214,18 +214,59 @@ export class WhatsAppController{
 
             this._camera = new CameraController(this.el.videoCamera);
             
+            this.el.inputDocument.click();
+            
+        })
+
+        this.el.inputDocument.on('change', e=>{
+
+            if(this.el.inputDocument.files.length){
+
+                let file=this.el.inputDocument.files[0];
+                console.log(file);
+
+            }
+
+            
+
         })
 
         this.el.btnClosePanelCamera.on('click', e=>{
 
             this.closeMainPanel();
             this.el.panelMessagesContainer.show();
+            this._camera.stopCamera();
 
         })
 
         this.el.btnTakePicture.on('click', e=>{
 
-            console.log('take picture')
+            let dataURL = this._camera.takePicture()
+
+            this.el.pictureCamera.src=dataURL;
+            this.el.pictureCamera.show();
+            this.el.videoCamera.hide();
+            this.el.btnReshootPanelCamera.show();
+            this.el.containerTakePicture.hide();
+            this.el.containerSendPicture.show();
+
+        })
+
+        this.el.btnReshootPanelCamera.on('click', e=>{
+
+            this.el.pictureCamera.hide();
+            this.el.videoCamera.show();
+            this.el.btnReshootPanelCamera.hide();
+            this.el.containerTakePicture.show();
+            this.el.containerSendPicture.hide();
+
+        })
+
+        this.el.btnSendPicture.on('click', e=>{
+
+
+            console.log(this.el.pictureCamera.src)
+
 
         })
 
